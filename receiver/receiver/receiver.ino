4857bc3497ba3925fc75d267df3296d0ce702208
calvinsuzuki <calvinssdcamargo@gmail.com>
2022-06-09 22:19:20 -0300

    revised receiver.ino

--- receiver/receiver/receiver.ino
+++ receiver/receiver/receiver.ino
@@ -1,10 +1,148 @@
 // Receptor - Paridade Par - Grupo
+// Caio Brandolim Rovetta | N USP:11232156
+// Calvin Suzuki de Camargo | N USP:11232420
+// Guilherme Soares Silvestre | N USP:11299832
 
 #define PINO_RX 7
 #define RTS 6  // emissor->receptor
 #define CTS 5  // receptor->emissor
 #define BAUD_RATE 1
 #define HALF_BAUD 1000 / (2 * BAUD_RATE)
+
+void configuraTemporizador(int);
+void iniciaTemporizador();
+void paraTemporizador();
+bool bitParidade(char);
+void transmissionError();
+char bufferToChar(int[]);
+
+int current_bit = 0;  // bit sendo tratado durante a execução
+bool flagTemporizador = false;
+char currentChar = '\0';  // variável auxiliar para ler o carácter que está
+                          // sendo transmitido durante a execução
+
+int buffer[8];
+bool stop_bit, parity, start_bit, check_pb;
+
+// Rotina de interrupção do timer1
+// O que fazer toda vez que 1s passou?
+// 0    1    2    3    4    5    6    7    8    9    10   (len = 11)
+// SB  LSB   D2   D3   D4   D5   D6   D7   MSB  PB   EB
+ISR(TIMER1_COMPA_vect) {
+    // Primeira iteração
+    if (current_bit == 0){
+        while (digitalRead(PINO_RX) == LOW){
+          start_bit = digitalRead(PINO_RX);
+          Serial.print("Start bit: ");
+          Serial.println(digitalRead(PINO_RX));
+          };
+    }
+    
+    // verificando os dados
+    else if (current_bit >= 1 && current_bit < 9){
+        buffer[current_bit-1] = digitalRead(PINO_RX);
+        Serial.print("Dado ");
+        Serial.print(current_bit);
+        Serial.print(": ");
+        Serial.println(digitalRead(PINO_RX));
+        }
+
+    // bit de paridade
+    else if (current_bit == 9) {
+        parity = digitalRead(PINO_RX);
+        Serial.print("Paridade ");
+        Serial.println(digitalRead(PINO_RX));
+        check_pb = bitParidade(buffer);
+        if (parity != check_pb)
+            transmissionError()
+    }
+
+    // verifica se houve o encerramento da transmissão
+    else if (current_bit == 10) {
+        stop_bit = digitalRead(PINO_RX);
+         Serial.print("Stop bit ");
+         Serial.println(digitalRead(PINO_RX));
+        paraTemporizador();
+    }
+    // se nao acabou o quadro (size=11), incremente o bit atual
+    if (current_bit <= 10) {
+        current_bit++;
+    } 
+    // se acabou o quadro, retorne o bit para 0
+    else {
+        current_bit = 0;
+    }
+}
+int binaryToDecimal(char binaryNumber[]) {
+    int dec = 0;
+    int pot = 1;
+    for ( int i = 0; i < 8; i ++ ) {
+      if( binaryNumber[7-i] == '1' ) {
+        for (int y = 0; y < i; y++ ) pot = pot * 2;
+        dec = dec + pot;
+        pot = 1;
+      }
+    }   
+    return dec;
+}
+
+char bufferToChar(int buffer[]) {
+    char aux[8];
+    // Swap data
+    for (int i = 0; i < 8; i++) aux[i] = buffer[7-i] + 48;
+    // Get char
+    char result = (char)binaryToDecimal(aux);
+    return result;
+}
+
+// Executada uma vez quando o Arduino reseta
+void setup() {
+    // Removendo as interrupções de clock
+    noInterrupts();
+
+    // Setando a porta serial (Serial Monitor - Ctrl + Shift + M)
+    Serial.begin(9600);
+
+    // Inicializando os pinos
+    pinMode(PINO_RX, INPUT);
+
+    // pins de handshake:
+    pinMode(RTS, INPUT);
+    pinMode(CTS, OUTPUT);
+    digitalWrite(CTS, LOW);
+
+    // Setando o temporizador
+    configuraTemporizador(BAUD_RATE);
+
+    // habilitando novamente interrupções
+    interrupts();
+}
+
+// O loop() eh executado continuamente (como um while(true))
+void loop() {
+    // RTS em HIGH????
+    if (digitalRead(RTS) == HIGH && digitalRead(CTS) == LOW) {
+        Serial.println("Transmission initialized. RTS is True. Setting CTS to True.");
+        digitalWrite(CTS, HIGH);
+        // Configura o contador de bit
+        current_bit = 0;
+        // Envia a confirmação do Request To Send
+        // Inicia a leitura
+        iniciaTemporizador();
+    }
+    // Fim da transmissão
+    else if (digitalRead(RTS) == LOW && digitalRead(CTS) == HIGH) {
+        Serial.print("Message received: ");
+        Serial.println(bufferToChar(buffer));
+        for (int i = 0; i<8; i++) buffer[i] = 0;
+        digitalWrite(CTS, LOW);
+        Serial.println("CTS set to False");
+        Serial.println("Transmission finished");
+        paraTemporizador();
+        current_bit = 0;
+    }
+}
+
 
 void configuraTemporizador(int baud_rate) {
     int frequencia;
@@ -41,108 +179,11 @@ void paraTemporizador() {
     TIMSK1 = 0;
 }
 
-int current_bit = 0;  // bit sendo tratado durante a execução
-bool flagTemporizador = false;
-char currentChar = '\0';  // variável auxiliar para ler o carácter que está
-                          // sendo transmitido durante a execução
-
-int buffer[8];
-bool stop_bit, parity, start_bit, check_pb;
-
-bool bitParidade(char);
-void transmissionError();
-char interpretBuffer(int[]);
-
-// Rotina de interrupção do timer1
-// O que fazer toda vez que 1s passou?
-
-// 0    1    2    3    4    5    6    7    8    9    10   (len = 11)
-// SB  LSB   D2   D3   D4   D5   D6   D7   MSB  PB   EB
-ISR(TIMER1_COMPA_vect) {
-    // Primeira iteração
-    if (current_bit == 0){
-        while (digitalRead(PINO_RX) == LOW){
-          start_bit = digitalRead(PINO_RX);
-          Serial.print("Start bit: ");
-          Serial.println(digitalRead(PINO_RX));
-          };
-    }
-    
-    // verificando os dados
-    else if (current_bit >= 1 && current_bit < 9){
-        buffer[current_bit-1] = digitalRead(PINO_RX);
-        Serial.print("Dado ");
-        Serial.print(current_bit);
-        Serial.print(": ");
-        Serial.println(digitalRead(PINO_RX));
-        }
-
-    // bit de paridade
-    else if (current_bit == 9) {
-        parity = digitalRead(PINO_RX);
-         Serial.print("Paridade ");
-         Serial.println(digitalRead(PINO_RX));
-        // check_pb = bitParidade(buffer);
-        // if (!check_pb) {
-        //     transmissionError() /
-        // }
-    }
-
-    // verifica se houve o encerramento da transmissão
-    else if (current_bit == 10) {
-        stop_bit = digitalRead(PINO_RX);
-         Serial.print("Stop bit ");
-         Serial.println(digitalRead(PINO_RX));
-        paraTemporizador();
-    }
-    //
-    if (current_bit <= 10) {
-        current_bit++;
-    } else {
-        current_bit = 0;
-    }
-}
-int binaryToDecimal(char binaryNumber[]) {
-    int dec = 0;
-    int pot = 1;
-    for ( int i = 0; i < 8; i ++ ) {
-      if( binaryNumber[7-i] == '1' ) {
-        for (int y = 0; y < i; y++ ) {
-          pot = pot * 2;
-        }
-          dec = dec + pot;
-          pot = 1;
-      }
-    }   
-    return dec;
-}
-
-char interpretBuffer(int buffer[]) {
-    char aux[8];
-    // Swap data
-    for (int i = 0; i < 8; i++) aux[i] = buffer[7-i] + 48;
-    // Get char
-    char result = (char)binaryToDecimal(aux);
-    // Verify data integrity
-//    if ((int)bitParidade(result) == buffer[9]) {
-//        Serial.println("Brabo");
-//        return result;
-//    } else {
-//        Serial.println("Not brabo");
-//        transmissionError();
-//        return (char)21;  // [NEGATIVE ACKNOWLEDGE]
-//    }
-    return result;
-}
-
-int t0 = millis();
-
 // Calcula bit de paridade - Par ou impar
 bool bitParidade(char dado) {
     int count = 0;
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) 
         count += bitRead(dado, i);
-    }
     return !(count % 2);  // par -> resto 0 -> complemento de 0 -> 1
 }
 
@@ -152,62 +193,4 @@ void transmissionError() {
     digitalWrite(CTS, LOW);
     paraTemporizador();
     current_bit = 0;
-}
-
-
-// Executada uma vez quando o Arduino reseta
-void setup() {
-    // Removendo as interrupções de clock
-    noInterrupts();
-
-    // Setando a porta serial (Serial Monitor - Ctrl + Shift + M)
-    Serial.begin(9600);
-
-    // Inicializando os pinos
-    pinMode(PINO_RX, INPUT);
-
-    // pins de handshake:
-    pinMode(RTS, INPUT);
-    pinMode(CTS, OUTPUT);
-
-    digitalWrite(CTS, LOW);
-
-    // Setando o temporizador
-    configuraTemporizador(BAUD_RATE);
-
-    // habilitando novamente interrupções
-    interrupts();
-}
-
-// O loop() eh executado continuamente (como um while(true))
-void loop() {
-//    float t = millis();
-//    if (t-t0>500){
-//        Serial.print("RTS: ");
-//        Serial.println(digitalRead(RTS));
-//        Serial.print("CTS: ");
-//        Serial.println(digitalRead(CTS));
-//        t0 = millis();
-//    }
-    // RTS em HIGH????
-    if (digitalRead(RTS) == HIGH && digitalRead(CTS) == LOW) {
-        Serial.println("Transmission initialized. RTS is True. Setting CTS to True.");
-        digitalWrite(CTS, HIGH);
-        // Configura o contador de bit
-        current_bit = 0;
-        // Envia a confirmação do Request To Send
-        // Inicia a leitura
-        iniciaTemporizador();
-    }
-    // Fim da transmissão
-    else if (digitalRead(RTS) == LOW && digitalRead(CTS) == HIGH) {
-        Serial.print("Message received: ");
-        Serial.println(interpretBuffer(buffer));
-        for (int i = 0; i<8; i++) buffer[i] = 0;
-        digitalWrite(CTS, LOW);
-        Serial.println("CTS set to False");
-        Serial.println("Transmission finished");
-        paraTemporizador();
-        current_bit = 0;
-    }
 }
